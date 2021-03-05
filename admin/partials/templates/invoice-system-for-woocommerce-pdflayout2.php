6057e6c5493ee4986d877a2bf661fd78b73ab174
vivek kushwaha <vivekkushwaha@makewebbetter.com>
2021-03-05 12:55:05 +0530

    two layout for pdf genaration

---
+++ admin/partials/templates/invoice-system-for-woocommerce-pdflayout2.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This is template one for the pdf generation.
+ *
+ * @package invoice-system-forwoocommerce.
+ *
+ * @return void
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+/**
+ * Return the html data for the second template.
+ *
+ * @return string
+ */
+function return_ob_value() {
+	$html = 'hello';
+	return $html;
+}
