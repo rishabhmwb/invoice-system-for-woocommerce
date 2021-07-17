d69e08939080a37c1fcdb8959af7fada41438405
vivek kushwaha <vivekkushwaha@makewebbetter.com>
2021-07-17 18:47:57 +0530

    readme updated

--- invoice-system-for-woocommerce.php
+++ invoice-system-for-woocommerce.php
@@ -24,7 +24,7 @@
  * Requires at least:    4.6
  * Tested up to:         5.7
  * WC requires at least: 4.0.0
- * WC tested up to:      5.1
+ * WC tested up to:      5.5.1
  * Stable tag:           1.0.1
  * Requires PHP:         7.2
  *
