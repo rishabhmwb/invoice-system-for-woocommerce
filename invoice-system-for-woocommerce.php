af62d6f10763c25dd8d5d96e9a44e76cba47f7f8
vivek kushwaha <vivekkushwaha@makewebbetter.com>
2021-09-06 13:21:41 +0530

    readme file updated

--- invoice-system-for-woocommerce.php
+++ invoice-system-for-woocommerce.php
@@ -26,6 +26,7 @@
  * WC requires at least: 4.0.0
  * WC tested up to:      5.6.0
  * Stable tag:           1.0.3
+ * Requires PHP:         7.2
  *
  * License:           GNU General Public License v3.0
  * License URI:       http://www.gnu.org/licenses/gpl-3.0.html
